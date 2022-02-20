3a29c176b375ce09ad7ff87297c0186deca8de47
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2022-02-20 10:04:33 +0900

    add: prettier and eslint

--- jest-testing-library/color-button/src/App.js
+++ jest-testing-library/color-button/src/App.js
@@ -1,28 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export function replaceCamelWithSpaces(colorName) {
-  return colorName.replace(/\B([A-Z])\B/g, ' $1')
-
+  return colorName.replace(/\B([A-Z])\B/g, ' $1');
 }
 
 const App = () => {
   const [buttonColor, setButtonColor] = useState('MediumVioletRed');
-  const newButtonColor = buttonColor === 'MidnightBlue' ? 'MediumVioletRed': 'MidnightBlue';
+  const newButtonColor =
+    buttonColor === 'MidnightBlue' ? 'MediumVioletRed' : 'MidnightBlue';
   const [isDisabled, setIsDisabled] = useState(false);
 
   return (
     <div>
-      <button style={{ backgroundColor: isDisabled ? 'gray' : buttonColor }} disabled={isDisabled} onClick={() => setButtonColor(newButtonColor)}>Change to {replaceCamelWithSpaces(newButtonColor)}</button>
+      <button
+        style={{ backgroundColor: isDisabled ? 'gray' : buttonColor }}
+        disabled={isDisabled}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change to {replaceCamelWithSpaces(newButtonColor)}
+      </button>
       <input
         id="disable-button-checkbox"
         type="checkbox"
-        onClick={() =>
-          setIsDisabled(!isDisabled)}
-        onChange={e => setIsDisabled(e.target.checked)}
+        onClick={() => setIsDisabled(!isDisabled)}
+        onChange={(e) => setIsDisabled(e.target.checked)}
       />
       <label htmlFor="disable-button-checkbox">Disable Button</label>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
